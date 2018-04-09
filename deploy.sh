28ad21e6d5353951e064a96d04254938c4a940cf
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-04-09 08:58:25 +0200

    fix: Add registry setup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# Create private image registry on our swarm
+if [ ! "$(docker service ls | grep registry)" ]; then
+  docker service create -d \
+    --name registry \
+    -p 5000:5000 \
+    --mount type=volume,source=registry,destination=/var/lib/registry,volume-driver=local \
+    registry:latest
+fi
+
+# Deploy
 docker stack deploy --compose-file docker/docker-compose.yml ci
 docker service logs ci_agent -f
